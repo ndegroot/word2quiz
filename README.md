ab1532c87afa7830ee78ba294c985b151cc9949f
Nico de Groot <ndegroot0@gmail.com>
2022-04-29 07:58:01 +0200

    working on GUI

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # word2quiz
-Create quizzes in Canvas from simple Word docx uaing Canvasapi
-A library to use in webapp, commandl ine or gui progrsm
+Create quizzes in Canvas from simple Word docx files uaing Canvasapi.
+A library to use in webapp, commandline or gui program. I will add the canvasapi part and make 
+it into a standalone tool
 
 
--- tests/test_word2quiz.py
+++ tests/test_word2quiz.py
@@ -1,31 +1,40 @@
 """ tests for word2quiz"""
 from word2quiz.main import parse
+from word2quiz.main import normalize_size
 
 
 def test_q_or_a_simple():
     """simple version no fonts size in Q & A"""
     src = r'<span style="font-size:32pt"><u>De titel</u>'
-    qa_id, weight, text, p_type, fontsize = parse(src)
+    qa_id, weight, text, p_type = parse(src)
     assert qa_id in (None, "")
     assert weight == 0
     assert text == "De titel"
     assert p_type == "Title"
-    assert fontsize == '32pt'
+    # assert fontsize == '32pt'
 
-    qa_id, weight, text, p_type, fontsize = parse('<font size="32"><u>De titel</u></font>')
+    qa_id, weight, text, p_type = parse('<font size="32"><u>De titel</u></font>')
     assert qa_id in (None, "")
     assert weight == 0
     assert text == "De titel"
     assert p_type == "Title"
-    assert fontsize == 32
-    question_nr, weight, text, p_type, fontsize = parse('1) fdfdsfffsd')
+
+    src = r'<span style="font-size:32pt"><b>Quiznaam</b></span>'
+    qa_id, weight, text, p_type = parse(src)
+    assert qa_id in (None, "")
+    assert p_type == "Quizname"
+    assert weight == 0
+    assert text == "Quiznaam"
+    # assert fontsize == '32pt'
+    # assert fontsize == 32
+    question_nr, weight, text, p_type = parse('1) fdfdsfffsd')
     assert p_type == "Question"
     assert question_nr == 1
-    question_nr, weight, text, p_type, fontsize = parse('a) answer')
+    question_nr, weight, text, p_type = parse('a) answer')
     assert p_type == 'Answer' and weight == 0
-    question_nr, weight, text, p_type, fontsize = parse('a) !answer')
+    question_nr, weight, text, p_type = parse('a) !answer')
     assert p_type == 'Answer' and weight == 100
-    question_nr, weight, text, p_type, fontsize = \
+    question_nr, weight, text, p_type = \
         parse('d)	!liturgieën een bredere werkelijkheid bestrijken '
               'dan de zeven sacramenten die door de Kerk erkend worden')
     assert p_type == 'Answer' and weight == 100
@@ -34,19 +43,52 @@ def test_q_or_a_simple():
 def test_q_or_a_fontsize():
     """font size versions"""
     src = '61)	<font size="24">αἱ ἀδελφαὶ αὐτῆς </font>'
-    question_nr, weight, text, p_type, fontsize = parse(src)
+    question_nr, weight, text, p_type = parse(src)
+    assert p_type == "Question"
     assert question_nr == 61
-    assert text == 'αἱ ἀδελφαὶ αὐτῆς'
-    assert p_type == "Question" and fontsize == 24
-    question_nr, weight, text, p_type, fontsize = parse('a) <font size="24">haar zusters</font>')
-    assert p_type == 'Answer' and weight == 0 and fontsize == 24
-    question_nr, weight, text, p_type, fontsize = parse('a) <font size="24">!haar zusters</font>')
-    assert p_type == 'Answer' and weight == 100 and fontsize == 24
-    question_nr, weight, text, p_type, fontsize = \
+    assert text == '<font size="24">αἱ ἀδελφαὶ αὐτῆς </font>'
+    question_nr, weight, text, p_type = parse('a) <font size="24">haar zusters</font>')
+    assert p_type == 'Answer' and weight == 0
+    question_nr, weight, text, p_type = parse('a) <font size="24">!haar zusters</font>')
+    assert p_type == 'Answer' and weight == 100
+    assert text == '<font size="24">haar zusters</font>'
+    question_nr, weight, text, p_type = \
         parse('d)	!liturgieën een bredere werkelijkheid bestrijken '
               'dan de zeven sacramenten die door de Kerk erkend worden')
     assert p_type == 'Answer' and weight == 100
 
+
+def test_normalize_size():
+
+    qa_size = 12
+
+    # quiz_name = 'VRAGEN BIJ HOOFDSTUK 1'
+    # tobe_q = 'VRAGEN BIJ HOOFDSTUK 1'
+
+    html_fs = '<span style="font-size:24pt">onze πάτερ zusters</span>'
+
+    html_tags = 'onze <i>πάτερ</i> zusters'
+    tobe_tags = f'<span style="font-size:{qa_size}pt">onze <i>πάτερ</i> zusters</span>'
+
+    html_not_really = 'onze πάτερ zusters'
+    tobe = f'<span style="font-size:{qa_size}pt">onze πάτερ zusters</span>'
+
+    print(f"\n\nWe have tested:\n{tobe}")
+
+    # result_q = normalize_size(quiz_name, qa_size)
+    # assert result_q == tobe_q, f"normalize quiz_name fails! ={result_q}"
+
+    print(tobe)
+    result_fs = normalize_size(html_fs, qa_size)
+    assert result_fs == tobe, f"normalize html_fs fails! ={result_fs}"
+
+    print(tobe_tags)
+    result_tags = normalize_size(html_tags, qa_size)
+    assert result_tags == tobe_tags, f"normalize html_tags fails! ={result_tags}"
+
+    print(tobe)
+    result_not_really = normalize_size(html_not_really, qa_size)
+    assert result_not_really == tobe, f"normalize html_not_really fails! ={result_not_really}"
 
 # def test_version():
 #     assert __version__ == '0.1.1'
